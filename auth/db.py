bb300faf2fd85eb8fc3b7f2c25662857b6d7bb50
Gandlin Alexander <137958267+GandlinAlexandr@users.noreply.github.com>
2025-03-24 06:19:43 +0300

    Add files via upload

---
+++ auth/db.py
@@ -0,0 +1,30 @@
+from collections.abc import AsyncGenerator
+from fastapi import Depends
+from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+import config
+from database import Base
+
+DATABASE_URL = config.DATABASE_URL
+
+
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    __tablename__ = "user"
+
+
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
