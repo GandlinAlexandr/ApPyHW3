fbc20f40de57d53cfa98b769498d646c100644c7
Gandlin Alexander <137958267+GandlinAlexandr@users.noreply.github.com>
2025-03-24 06:27:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,11 @@ REDIS_HOST = os.getenv("REDIS_HOST")
 
 CLEANUP_AFTER_DAYS = int(os.getenv("CLEANUP_AFTER_DAYS", 30))
 
-ASYNC_DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-SYNC_DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+ASYNC_DATABASE_URL = (
+    f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+SYNC_DATABASE_URL = (
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
 DATABASE_URL = ASYNC_DATABASE_URL
