51c09e02d7ffa8a09c957bac9faec016c927cf0e
Gandlin Alexander <137958267+GandlinAlexandr@users.noreply.github.com>
2025-03-24 06:28:20 +0300

    Update database.py

--- database.py
+++ database.py
@@ -17,6 +17,7 @@ SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=F
 class Base(DeclarativeBase):
     pass
 
+
 # Модель для хранения коротких ссылок
 class Link(Base):
     __tablename__ = "links"
@@ -31,6 +32,7 @@ class Link(Base):
     owner_id = Column(UUID(as_uuid=True), ForeignKey("user.id"), nullable=True)
     project_id = Column(Integer, ForeignKey("projects.id"), nullable=True)
 
+
 # Модель для хранения мёртвых ссылок
 class ExpiredLink(Base):
     __tablename__ = "expired_links"
@@ -44,6 +46,7 @@ class ExpiredLink(Base):
     access_count = Column(Integer, default=0)
     owner_id = Column(UUID(as_uuid=True), ForeignKey("user.id"), nullable=False)
     project_id = Column(Integer, ForeignKey("projects.id"), nullable=True)
+
 
 # Модель для хранения проектов
 class Project(Base):
@@ -60,10 +63,12 @@ async def get_db():
     async with SessionLocal() as session:
         yield session
 
+
 # Асинхронная функция инициализации базы данных
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
 
 async def get_async_session():
     async for s in get_db():
